649572f7ada0adb828548afefc6010df6f28c4ad
JP Salado <jpsalado92@gmail.com>
2022-03-04 14:44:52 +0100

    Update get_cont.py

--- scripts/get_cont.py
+++ scripts/get_cont.py
@@ -3,7 +3,7 @@ import os
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from io import BytesIO
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
 
 import requests
 
@@ -13,8 +13,8 @@ from utils import retry
 SCOPE = "contratos"
 DATA_PATH = os.path.join(os.getcwd(), '..', 'data', SCOPE)
 TIME_STAMP = datetime.now().strftime("%Y%m%d")
+RAW_PA_CONTS_PATH = os.path.join(DATA_PATH, 'raw_pa_conts', TIME_STAMP)
 BASE_URL = "https://www.contratacion.euskadi.eus/"
-
 COOKIES_URL = BASE_URL + "w32-kpetrans/es/ac70cPublicidadWar" \
                          "/busquedaInformesOpenData" \
                          "?locale=es"
@@ -37,43 +37,39 @@ XML_LIST_FIELDS = (
     'lugaresAplicacion', 'resoluciones', 'publicacionesDOUE')
 
 
-def get_raw_data_files():
+def get_raw_cont_files():
     """ Fetch and store `xml` and `json` CONT data locally """
     for pa_d in get_pa_dict_list():
         pa_cod_perfil = pa_d['codPerfil']
 
-        conts_by_pa = get_cont_list_by_pa(pa_cod_perfil=pa_cod_perfil)
-        if not conts_by_pa:
-            continue
-
-        # Directory in which this PAs CONTs will be stored
-        raw_pa_conts_path = os.path.join(DATA_PATH, 'raw_pa_conts', TIME_STAMP)
-
-        for cont in conts_by_pa:
+        for cont in get_cont_list_by_pa(pa_cod_perfil=pa_cod_perfil):
             cont_date = str(int(cont['anioInforme']))
             cont_mod_date = cont['fechaModif'].replace('-', '')
             cont_id = str(int(cont['idInformeOpendata']))
+            cont_fname = f"{int(pa_cod_perfil):05d}_{cont_date}_{cont_id}_{cont_mod_date}"
 
-            # Store individual json data
-            json_fname = f"{int(pa_cod_perfil):05d}_{cont_date}_{cont_id}_{cont_mod_date}.json"
-            cont_fpath = os.path.join(raw_pa_conts_path, json_fname.removesuffix('.json'))
+            cont_fpath = os.path.join(RAW_PA_CONTS_PATH, cont_fname)
+            if os.path.isdir(cont_fpath):
+                continue
+
             os.makedirs(cont_fpath, exist_ok=True)
-            json_fpath = os.path.join(cont_fpath, json_fname)
-            if not os.path.isfile(json_fpath):
-                with open(json_fpath, mode='w', encoding='utf8') as file:
-                    del cont['poder']
-                    file.write(json.dumps(cont, indent=2, ensure_ascii=False))
+            json_fpath = os.path.join(cont_fpath, cont_fname + '.json')
+            with open(json_fpath, mode='w', encoding='utf8') as file:
+                del cont['poder']
+                file.write(json.dumps(cont, indent=2, ensure_ascii=False))
 
             # Fetch, store and extract zip file
-            r = requests.get(CONT_URL.format(codperfil=pa_cod_perfil, report_date=cont_date))
-            xml_fpath = json_fpath.removesuffix('.json') + '.xml'
-            if not os.path.isfile(xml_fpath):
+            zip_url = CONT_URL.format(codperfil=pa_cod_perfil, report_date=cont_date)
+            try:
+                r = requests.get(zip_url)
+                xml_fpath = os.path.join(cont_fpath, cont_fname + '.xml')
                 with ZipFile(BytesIO(r.content)) as zfile:
                     for file in zfile.namelist():
                         if file.endswith('.xml'):
-                            zname = file.replace('"', '_')
                             zfile.extract(file, cont_fpath)
-                            os.rename(os.path.join(cont_fpath, zname), xml_fpath)
+                            os.rename(os.path.join(cont_fpath, file.replace('"', '_')), xml_fpath)
+            except BadZipFile:
+                print(zip_url)
 
 
 @retry(times=5, exceptions=json.decoder.JSONDecodeError, sleep=0.5)
@@ -102,18 +98,18 @@ def get_cont_list_by_pa(pa_cod_perfil):
     return r_json["rows"]
 
 
-def get_cont_file(date):
-    consolidated_cont_fpath = os.path.join(DATA_PATH, f"{date}_contratos.jsonl")
-    with open(consolidated_cont_fpath, 'w') as contfile:
+def get_cont_file():
+    with open(os.path.join(DATA_PATH, f"{TIME_STAMP}_contratos.jsonl"), 'w') as contfile:
         # Iterating through every CONT directory
-        raw_data_path = os.path.join(DATA_PATH, 'raw_pa_conts', date)
-        for dirname in os.listdir(raw_data_path):
-            dir_path = os.path.join(raw_data_path, dirname)
+        for dirname in os.listdir(RAW_PA_CONTS_PATH):
+            dir_path = os.path.join(RAW_PA_CONTS_PATH, dirname)
             pa_cod_perfil, cont_date, cont_id, cont_mod_date = dirname.split('_')
             pa_cod_perfil = str(int(pa_cod_perfil))
-            cont_d = {"cont_date": cont_date, "pa_cod_perfil": pa_cod_perfil, "cont_id": cont_id,
-                      "cont_mod_date": cont_mod_date}
+            cont_d = {"cont_date": cont_date, "pa_cod_perfil": pa_cod_perfil,
+                      "cont_id": cont_id, "cont_mod_date": cont_mod_date}
             # Iterating through every CONT in a given `xml` file
+            if len(os.listdir(dir_path)) != 2:
+                continue
             for xml_cont in ET.parse(os.path.join(dir_path, dirname + '.xml')).getroot():
                 parsed_cont_d = parse_xml_field(node=xml_cont, dict_obj=cont_d.copy())
                 parsed_cont_d["codExp"], *_ = parsed_cont_d["codContrato"].split('_')
@@ -183,5 +179,5 @@ def clean_xml_text(text):
 
 
 if __name__ == "__main__":
-    get_raw_data_files()
-    get_cont_file(date="20220303")
+    get_raw_cont_files()
+    get_cont_file()
